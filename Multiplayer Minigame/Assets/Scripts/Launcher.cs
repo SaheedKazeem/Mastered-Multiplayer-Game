29565078f5ac7a603cf520feff44c03478765742
SaheedKazeem <saheekazeem@gmail.com>
2023-04-30 12:47:49 +0100

    Progress Label done

--- Multiplayer Minigame/Assets/Scripts/Launcher.cs
+++ Multiplayer Minigame/Assets/Scripts/Launcher.cs
@@ -17,6 +17,12 @@ namespace Com.MyCompany.MyGame
         #endregion
 
         #region Private Fields
+        [Tooltip("The Ui Panel to let the user enter name, connect and play")]
+        [SerializeField]
+        private GameObject controlPanel;
+        [Tooltip("The UI Label to inform the user that the connection is in progress")]
+        [SerializeField]
+        private GameObject progressLabel;
 
         /// <summary>
         /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
@@ -42,7 +48,8 @@ namespace Com.MyCompany.MyGame
         /// </summary>
         void Start()
         {
-            
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
         }
 
         #endregion
@@ -57,6 +64,8 @@ namespace Com.MyCompany.MyGame
         /// </summary>
         public void Connect()
         {
+            progressLabel.SetActive(true);
+            controlPanel.SetActive(false);
             // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
             if (PhotonNetwork.IsConnected)
             {
@@ -74,6 +83,17 @@ namespace Com.MyCompany.MyGame
         #endregion
 
         #region MonoBehaviourPunCallbacks Callbacks
+        public override void OnConnectedToMaster()
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
+        }
+
+        public override void OnDisconnected(DisconnectCause cause)
+        {
+            progressLabel.SetActive(false);
+            controlPanel.SetActive(true);
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+        }
 
         public override void OnJoinRandomFailed(short returnCode, string message)
         {
