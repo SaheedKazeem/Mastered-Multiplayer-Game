f3b212c28039075bbdc744feeeb3b32db177935d
SaheedKazeem <saheekazeem@gmail.com>
2023-04-30 15:18:08 +0100

    Revert "imported dino"
    This reverts commit f6892d73d090931fe367b6f284992ea0b3e0c434.

--- Multiplayer Minigame/Assets/Scripts/GameManager.cs
+++ Multiplayer Minigame/Assets/Scripts/GameManager.cs
@@ -1,101 +1,78 @@
-using TMPro;
+using System;
+using System.Collections;
+
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-[DefaultExecutionOrder(-1)]
-public class GameManager : MonoBehaviour
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace Com.MyCompany.MyGame
 {
-    public static GameManager Instance { get; private set; }
-
-    public float initialGameSpeed = 5f;
-    public float gameSpeedIncrease = 0.1f;
-    public float gameSpeed { get; private set; }
-
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI hiscoreText;
-    public TextMeshProUGUI gameOverText;
-    public Button retryButton;
-
-    private Player player;
-    private Spawner spawner;
-
-    private float score;
-
-    private void Awake()
+    public class GameManager : MonoBehaviourPunCallbacks
     {
-        if (Instance != null) {
-            DestroyImmediate(gameObject);
-        } else {
-            Instance = this;
-        }
-    }
 
-    private void OnDestroy()
-    {
-        if (Instance == this) {
-            Instance = null;
-        }
-    }
+        #region Photon Callbacks
 
-    private void Start()
-    {
-        player = FindObjectOfType<Player>();
-        spawner = FindObjectOfType<Spawner>();
-
-        NewGame();
-    }
-
-    public void NewGame()
-    {
-        Obstacle[] obstacles = FindObjectsOfType<Obstacle>();
-
-        foreach (var obstacle in obstacles) {
-            Destroy(obstacle.gameObject);
-        }
-
-        score = 0f;
-        gameSpeed = initialGameSpeed;
-        enabled = true;
-
-        player.gameObject.SetActive(true);
-        spawner.gameObject.SetActive(true);
-        gameOverText.gameObject.SetActive(false);
-        retryButton.gameObject.SetActive(false);
-
-        UpdateHiscore();
-    }
-
-    public void GameOver()
-    {
-        gameSpeed = 0f;
-        enabled = false;
-
-        player.gameObject.SetActive(false);
-        spawner.gameObject.SetActive(false);
-        gameOverText.gameObject.SetActive(true);
-        retryButton.gameObject.SetActive(true);
-
-        UpdateHiscore();
-    }
-
-    private void Update()
-    {
-        gameSpeed += gameSpeedIncrease * Time.deltaTime;
-        score += gameSpeed * Time.deltaTime;
-        scoreText.text = Mathf.FloorToInt(score).ToString("D5");
-    }
-
-    private void UpdateHiscore()
-    {
-        float hiscore = PlayerPrefs.GetFloat("hiscore", 0);
-
-        if (score > hiscore)
+        /// <summary>
+        /// Called when the local player left the room. We need to load the launcher scene.
+        /// </summary>
+        public override void OnLeftRoom()
         {
-            hiscore = score;
-            PlayerPrefs.SetFloat("hiscore", hiscore);
+            SceneManager.LoadScene(0);
         }
 
-        hiscoreText.text = Mathf.FloorToInt(hiscore).ToString("D5");
+        #endregion
+
+        #region Public Methods
+
+        public void LeaveRoom()
+        {
+            PhotonNetwork.LeaveRoom();
+        }
+
+        #endregion
+        #region Private Methods
+
+        void LoadArena()
+        {
+            if (!PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+                return;
+            }
+            Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
+            PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
+        }
+
+        #endregion
+        #region Photon Callbacks
+
+        public override void OnPlayerEnteredRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+        public override void OnPlayerLeftRoom(Player other)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
+
+                LoadArena();
+            }
+        }
+
+        #endregion
     }
 
 }
